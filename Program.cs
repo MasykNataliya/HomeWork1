447a0eaeba41619306ca0e6bbb324dba7470c39d
MasykNataliya <masyknataliya@gmail.com>
2023-07-18 16:56:19 +0300

    Home Tasks

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFigure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int figure=0;
+            double perimeter = 0.0;
+            double square = 0.0;
+            Console.WriteLine("Оберіть фігуру: 1- коло, 2- чотирикутник, 3- трикутник");
+            bool valid = int.TryParse(Console.ReadLine().Trim(), out int input);
+            if (!valid||input>3||input<=0) Console.WriteLine("Не вірно обрана фігура");
+            else figure = input;
+            switch (figure)
+            {
+                case 1:
+                    double radius=0;
+                    Console.Write(" Введіть радіус кола, см");
+                    bool valid1 = double.TryParse(Console.ReadLine().Trim(), out double input1);
+                    if (!valid1 || input1 <= 0) Console.WriteLine("Не вірно введені дані. Потрібне число");
+                    else
+                    {
+                        radius = input1;
+                        double Pi;
+                        Pi = 3.1415926;
+                        perimeter = 2 * Pi * radius;
+                        square = Pi * (radius * radius);
+                        Console.WriteLine($"Площа кола = {square} см; периметр = {perimeter}см");
+                    }
+                    break;
+                case 2:
+                    double OneSide=0.0;
+                    double SecondSide=0.0;
+                    Console.WriteLine(" Введіть першу сторону чотирикутника, см");
+                    bool valid2 = double.TryParse(Console.ReadLine().Trim(), out double input2);
+                    if (!valid2 || input2 <= 0) { Console.WriteLine("Не вірно введені дані. Потрібне число"); }
+                    else
+                    {
+                        OneSide = input2;
+                        Console.WriteLine(" Введіть другу сторону чотирикутника, см");
+                        bool valid3 = double.TryParse(Console.ReadLine().Trim(), out double input3);
+                        if (!valid3 || input3 <= 0) { Console.WriteLine("Не вірно введені дані. Потрібне число"); }
+                        else
+                        {
+                            SecondSide = input3;
+                            perimeter = 2 * OneSide + 2 * SecondSide;
+                            square = OneSide * SecondSide;
+                            Console.WriteLine($"Площа чотирикутника = {square} см; периметр = {perimeter} см");
+                        }
+                    }
+                    break;
+                case 3:
+                    double OneSideT = 0.0;
+                    double SecondSideT = 0.0;
+                    double ThirdSideT = 0.0;
+                    Console.WriteLine(" Введіть першу сторону трикутника, см");
+                    bool valid4 = double.TryParse(Console.ReadLine().Trim(), out double input4);
+                    if (!valid4 || input4 <= 0) { Console.WriteLine("Не вірно введені дані. Потрібне число"); }
+                    else
+                    {
+                        OneSideT = input4;
+                        Console.WriteLine(" Введіть другу сторону трикутника, см");
+                        bool valid5 = double.TryParse(Console.ReadLine().Trim(), out double input5);
+                        if (!valid5 || input5 <= 0) Console.WriteLine("Не вірно введені дані. Потрібне число");
+                        else
+                        {
+                            SecondSideT = input5;
+                            Console.WriteLine(" Введіть третю сторону трикутника, см");
+                            bool valid6 = double.TryParse(Console.ReadLine().Trim(), out double input6);
+                            if (!valid6 || input6 <= 0) { Console.WriteLine("Не вірно введені дані. Потрібне число"); }
+                            else
+                            {
+                                ThirdSideT = input6;
+
+                                if (OneSideT + SecondSideT < ThirdSideT && OneSideT + ThirdSideT < SecondSideT && SecondSideT + ThirdSideT < OneSideT)
+                                {
+                                    perimeter = OneSideT + SecondSideT + ThirdSideT;
+                                    double DivTwoPerimeter = perimeter / 2 * (perimeter / 2 - OneSideT) * (perimeter / 2 - SecondSideT) * (perimeter / 2 - ThirdSideT);
+                                    square = Math.Sqrt(DivTwoPerimeter);
+                                    Console.WriteLine($"Площа трикутника = {square} см; периметр = {perimeter} см");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Не вірно задані сторони. Такого трикутника не існує");
+                                }
+                            }
+                        }
+                    }
+                        break;
+                    
+                
+
+            }
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ conditionsPractice/ConditionPracticeClass.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace conditionsPractice
+{
+    public class ConditionPracticeClass
+    {
+        /*TODO: The method must receive one parameter of the int type as input - a number to check.
+        The method must return a boolean value, depending on whether the input parameter is even or odd*/
+        public static bool IsEven(int number)
+        {
+              bool result;
+               result = (number % 2 == 0);
+            return result;
+        } 
+            
+
+        /* TODO: Receives a string as input and returns true if the string contains only unique characters, false otherwise. */
+        public static bool HasUniqueCharacters(string input)
+        {
+            string dist = (string)input.ToArray().Distinct();
+            if (dist == input)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /* TODO: Receives an integer as input and returns true if the number is divisible by both 5 and 7, excluding zero, false otherwise. */
+        public static bool IsDivisibleBy5And7(int number)
+        {
+            if (number % (5 * 7) == 0 && number != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /* TODO: Performs the specified arithmetic operation (char input paramether) on two double input paramethers and returns the result. */
+        public static double PerformOperation(char operation, double number1, double number2)
+        {
+            double result=0.0;
+            switch (operation)
+            {
+                case '-':
+                   result=( number1 - number2);
+                   
+                   break;
+                case '+':
+                   result = (number1 + number2);
+                      break;
+                case '*':
+                    result =  (number1 * number2);
+                    
+                    break;
+                case '/':
+                    result = (number1 / number2); 
+                        break;
+                default:
+                    Console.WriteLine("No such operation un the list");
+                    break;
+            }
+            return result;
+        }
+
+        /* TODO: Receives an integer as input and returns "Positive" if the number is positive, "Negative" if the number is negative, "Zero" if the number is zero. */
+        public static string CheckNumber(int number)
+        {
+            if(number < 0) return "Negative";
+                   
+              else if (number > 0) return "Positive";
+                  else  return "Zero";
+                   
+           
+        }
+
+        /* TODO: Receives a string as input and returns true if the string is a palindrome, false otherwise. */
+        public static bool IsPalindrome(string input)
+        { 
+        if (input == input.Reverse().ToString()) return true;
+        else return false;
+        }
+
+        /* TODO: Receives a int year as input paramether and returns true if the year is a leap year, false otherwise. */
+        public static bool IsLeapYear(int year)
+        {
+        bool result;
+            result = ((year % 4 == 0&& year % 100 != 0)|| (year % 400 == 0 ));
+            return result;
+        }
+
+
+        /* TODO: Receives a character as input and returns true if the character is a vowel, false otherwise. */
+        public static bool IsVowel(char character)
+        {
+            string character1 = character.ToString().ToLower();
+            if (character1 == "a" || character1 == "e" || character1 == "i" || character1 == "o" || character1 == "u")
+                return true;
+            else
+                return false;
+                }
+
+       
+
+        /* TODO: Converts a binary number represented as a string to its integer equivalent. */
+        public static int BinaryToDecimal(string binaryNumber)
+
+        { int QuantityOfIndexes = binaryNumber.Length;
+            string revBinaryNumber = binaryNumber.Reverse().ToString();
+            int result=0;
+            for (int i = 0; i <= QuantityOfIndexes; i++) 
+            { double DoubleIndex = Double.Parse(revBinaryNumber.Substring(QuantityOfIndexes - i, 1));
+                double inPow;
+                if (DoubleIndex > 0)
+                { inPow = Math.Pow(2, Convert.ToDouble(QuantityOfIndexes - i)); }
+                else inPow =0.0;
+                inPow = +inPow;
+               result= Convert.ToInt32(inPow);
+            }
+            return result;  }
+
+        /* TODO: Converts a binary number represented as a string to its hexadecimal string equivalent. */
+        public static string BinaryToHexadecimal(string binaryNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
